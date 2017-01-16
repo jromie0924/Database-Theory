8c443389e9705e4c2a1da7ccd0f02e44093f5f92
Jackson <jromie0924@gmail.com>
2017-01-16 16:48:50 -0500

    cleaned up code a little; progress on part D. C and E need to be thought out, then projections need made to show only the desired columns.

--- Relation.java
+++ Relation.java
@@ -12,78 +12,78 @@ public class Relation{
   public Relation(){ }
 
   public Relation(String filename){
-	Scanner in = null;
-	try {
-		in = new Scanner(new File(filename));
-	} catch (FileNotFoundException e){
-		System.err.println(filename + " not found.");
-		System.exit(1);
-	}
-	String[] terms = in.nextLine().split("\t");
-	name = terms[0];
-	cols = Integer.parseInt(terms[1]);
-	rows = Integer.parseInt(terms[2]);
-	attributes = new String[cols];
-	tuples = new String[rows][cols];
-	terms = in.nextLine().split("\t");
-	for (int c = 0; c < cols; c++) attributes[c] = terms[c];
-	for (int r = 0; r < rows; r++){
+		Scanner in = null;
+		try {
+			in = new Scanner(new File(filename));
+		} catch (FileNotFoundException e){
+			System.err.println(filename + " not found.");
+			System.exit(1);
+		}
+		String[] terms = in.nextLine().split("\t");
+		name = terms[0];
+		cols = Integer.parseInt(terms[1]);
+		rows = Integer.parseInt(terms[2]);
+		attributes = new String[cols];
+		tuples = new String[rows][cols];
 		terms = in.nextLine().split("\t");
-		for (int c = 0; c < cols; c++) tuples[r][c] = terms[c];
-	}
-	in.close();
+		for (int c = 0; c < cols; c++) attributes[c] = terms[c];
+		for (int r = 0; r < rows; r++){
+			terms = in.nextLine().split("\t");
+			for (int c = 0; c < cols; c++) tuples[r][c] = terms[c];
+		}
+		in.close();
   }
 
   public void showRelation(){
-	System.out.println(name + "\t" + cols + "\t" + rows);
-	System.out.print(attributes[0]);
-	for (int c = 1; c < cols; c++) System.out.print("\t" + attributes[c]);
-	System.out.println();
-	for (int r = 0; r < rows; r++){
-		System.out.print(tuples[r][0]);
-		for (int c = 1; c < cols; c++) System.out.print("\t" + tuples[r][c]);
+		System.out.println(name + "\t" + cols + "\t" + rows);
+		System.out.print(attributes[0]);
+		for (int c = 1; c < cols; c++) System.out.print("\t" + attributes[c]);
 		System.out.println();
-	}
+		for (int r = 0; r < rows; r++){
+			System.out.print(tuples[r][0]);
+			for (int c = 1; c < cols; c++) System.out.print("\t" + tuples[r][c]);
+			System.out.println();
+		}
   }
 
   public Relation project(String... attrs){
-	int[] colIndex = new int[attrs.length];
-	for (int c = 0; c < attrs.length; c++){
-		int j = 0; for (; j < cols; j++) if (attrs[c].equals(attributes[j])) break;
-		if (j == cols){
-			System.err.println("attribute " + attrs[c] + " not found.");
-			System.exit(1);
+		int[] colIndex = new int[attrs.length];
+		for (int c = 0; c < attrs.length; c++){
+			int j = 0; for (; j < cols; j++) if (attrs[c].equals(attributes[j])) break;
+			if (j == cols){
+				System.err.println("attribute " + attrs[c] + " not found.");
+				System.exit(1);
+			}
+			colIndex[c] = j;
 		}
-		colIndex[c] = j;
-	}
-	Relation PR = new Relation();
-	PR.cols = attrs.length;
-	PR.name = "project(" + name;
-	for (int c = 0; c < PR.cols; c++) PR.name += "," + attrs[c];
-	PR.name += ")";
-	PR.rows = rows;
-	PR.attributes = new String[PR.cols];
-	for (int c = 0; c < PR.cols; c++) PR.attributes[c] = attrs[c];
-	PR.tuples = new String[rows][PR.cols];
-	for (int r = 0; r < rows; r++)
-		for (int c = 0; c < PR.cols; c++) PR.tuples[r][c] = tuples[r][colIndex[c]];
-	return PR;
+		Relation PR = new Relation();
+		PR.cols = attrs.length;
+		PR.name = "project(" + name;
+		for (int c = 0; c < PR.cols; c++) PR.name += "," + attrs[c];
+		PR.name += ")";
+		PR.rows = rows;
+		PR.attributes = new String[PR.cols];
+		for (int c = 0; c < PR.cols; c++) PR.attributes[c] = attrs[c];
+		PR.tuples = new String[rows][PR.cols];
+		for (int r = 0; r < rows; r++)
+			for (int c = 0; c < PR.cols; c++) PR.tuples[r][c] = tuples[r][colIndex[c]];
+		return PR;
   }
 
   public Relation select(Function<Integer, Boolean> condition){
-	Relation SR = new Relation();
-	SR.cols= cols;
-	SR.name = "select(" + name + ",condition)";
-	int[] selected = new int[rows];
-	int n = 0;
-	for (int r = 0; r < rows; r++) if(condition.apply(r)) selected[n++] = r;
-	SR.rows = n;
-	SR.attributes = new String[cols];
-	for (int c = 0; c < cols; c++) SR.attributes[c] = attributes[c];
-	SR.tuples = new String[SR.rows][cols];
-	for (int r = 0; r < SR.rows; r++)
-		for (int c = 0; c < cols; c++) SR.tuples[r][c] = tuples[selected[r]][c];
-	return SR;
+		Relation SR = new Relation();
+		SR.cols= cols;
+		SR.name = "select(" + name + ",condition)";
+		int[] selected = new int[rows];
+		int n = 0;
+		for (int r = 0; r < rows; r++) if(condition.apply(r)) selected[n++] = r;
+		SR.rows = n;
+		SR.attributes = new String[cols];
+		for (int c = 0; c < cols; c++) SR.attributes[c] = attributes[c];
+		SR.tuples = new String[SR.rows][cols];
+		for (int r = 0; r < SR.rows; r++)
+			for (int c = 0; c < cols; c++) SR.tuples[r][c] = tuples[selected[r]][c];
+		return SR;
   }
 
   public Relation join(Relation other, Function<int[], Boolean> condition){
@@ -114,23 +114,54 @@ public class Relation{
 				JR.tuples[r][cols + c] = other.tuples[r2][c];
 			r++;
 		}
-	return JR;
+		return JR;
   }
+/*
+	public Relation union(Relation other) {
+		Relation unified = new Relation();
+		numRows = this.tuples.length + other.tuples.length;
+		numCols = this.tuples[0].length + other.tuples.length;
+		unified.tuples = new String[numRows][numCols];
 
+		// Fill new tuples
+		int a;
+		for(a = 0; a < this.tuples.length; a++) {
+			unified.tuples[a] = this.tuples[a];
+		}
+		for(int b = 0; b < other.tuples.length; b++) {
+			unified[a++] = other.tuples[b];
+		}
+
+		unified.name = this.name + " U " + other.name;
+	}
+*/
   public static void main(String[] args){
-	Relation Product = new Relation("files/Product.txt");
-	Relation PC = new Relation("files/PC.txt");
-	Relation Laptop = new Relation("files/Laptop.txt");
+		Relation product = new Relation("files/Product.txt");
+		Relation pc = new Relation("files/PC.txt");
+		Relation laptop = new Relation("files/Laptop.txt");
+		Relation printer = new Relation("files/Printer.txt");
 
-	System.out.println("Part A:\n");
-	Relation RA = PC.select(r -> Double.parseDouble(PC.tuples[r][1]) >= 3.0);
-	RA.join(Product, x -> RA.tuples[x[0]][0].equals(Product.tuples[x[1]][1])).showRelation();
-	// TODO: Projection for part (A)
+		System.out.println("Part A:\n");
+		Relation RA = pc.select(r -> Double.parseDouble(pc.tuples[r][1]) >= 3.0);
+		RA.join(product, x -> RA.tuples[x[0]][0].equals(product.tuples[x[1]][1])).showRelation();
+		// TODO: Projection for part (A)
 
-	System.out.println("---------------------------------------------------------------------\nPart B:\n");
-	Relation RB = Laptop.select(r -> Double.parseDouble(Laptop.tuples[r][3]) >= 100);
-	RB.join(Product, y -> RB.tuples[y[0]][0].equals(Product.tuples[y[1]][1])).showRelation();
-	// TODO: Projection for part (B)
-	
+		System.out.println("---------------------------------------------------------------------\nPart B:\n");
+		Relation RB = laptop.select(r -> Double.parseDouble(laptop.tuples[r][3]) >= 100);
+		RB.join(product, x -> RB.tuples[x[0]][0].equals(product.tuples[x[1]][1])).showRelation();
+		// TODO: Projection for part (B)
+
+		System.out.println("---------------------------------------------------------------------\nPart C:\n");
+		Relation manufacturerB = product.select(r -> product.tuples[r][0].equals("B"));
+		manufacturerB.join(laptop, x -> manufacturerB.tuples[x[0]][1].equals(laptop.tuples[x[1]][0]));
+		// TODO: finish part (C)
+
+		System.out.println("---------------------------------------------------------------------\nPart D:\n");
+		Relation colorPrinters = printer.select(r -> printer.tuples[r][1].equals("true"));
+		colorPrinters.showRelation();
+		// TODO: projection to show only the printer model numbers
+
+		
+
   }
 }
